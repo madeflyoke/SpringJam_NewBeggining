43ee0337f43f8b1448d868318c8e9680ac433873
madeflyoke <sano555000@gmail.com>
2024-04-22 21:23:12 +0300

    upd: campfires checkpoints

--- Assets/Content/Scripts/Game/ProgressHandler/Checkpoint.cs
+++ Assets/Content/Scripts/Game/ProgressHandler/Checkpoint.cs
@@ -7,17 +7,30 @@ namespace Content.Scripts.Game.ProgressHandler
     public class Checkpoint : MonoBehaviour
     {
         [SerializeField] private Transform respawnPoint;
+        [SerializeField] private ParticleSystem _activeParticles;
+        [SerializeField] private CharacterType _targetActivatorType;
         public Vector3 RespawnPoint => respawnPoint.position;
         public event Action<Checkpoint> OnPlayerReach;
         public bool Reached { get; private set; }
+
+        private void Start()
+        {
+            _activeParticles.gameObject.SetActive(false);
+            _activeParticles.Stop();
+        }
 
         private void OnTriggerEnter(Collider other)
         {
             if(!Reached)
                 if (other.gameObject.TryGetComponent(out Character character))
                 {
-                    Reached = true;
-                    OnPlayerReach?.Invoke(this);
+                    if (character.Type==_targetActivatorType)
+                    {
+                        Reached = true;
+                        _activeParticles.gameObject.SetActive(true);
+                        _activeParticles.Play();
+                        OnPlayerReach?.Invoke(this);
+                    }
                 }
             
         }
