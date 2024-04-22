23889d6a63ede04d3f747130088fd78a6a0229e4
madeflyoke <sano555000@gmail.com>
2024-04-22 04:32:21 +0300

    fix: small fixes

--- Assets/Content/Scripts/Game/Level/LevelFinishTrigger.cs
+++ Assets/Content/Scripts/Game/Level/LevelFinishTrigger.cs
@@ -6,12 +6,25 @@ namespace Content.Scripts.Game.Level
 {
     public class LevelFinishTrigger : MonoBehaviour
     {
+        [SerializeField] private Transform _requiredCharacter;
+        [SerializeField] private CharacterType _type;
+        
         public event Action OnPlayerWin;
 
         public void OnTriggerEnter(Collider other)
         {
-            if(other.gameObject.GetComponent<Character>())
-                OnPlayerWin?.Invoke();
+            if (other.gameObject.TryGetComponent(out Character otherChar))
+            {
+                if (otherChar.Type==_type)
+                {
+                    OnPlayerWin?.Invoke();
+                    _requiredCharacter.gameObject.SetActive(false);
+                }
+                else
+                {
+                    _requiredCharacter.gameObject.SetActive(true);
+                }
+            }
         }
     }
 }
